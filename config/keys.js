f5348f2b653422e2dc6d38ea29147a3d52a4fe27
irakli iakobishvili <irakli.iakobishvili@gmail.com>
2019-02-12 05:22:21 +0400

    Express with routes

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI: 'mongodb://irakli:irakli123@ecommerce-shard-00-00-pca9p.mongodb.net:27017,ecommerce-shard-00-01-pca9p.mongodb.net:27017,ecommerce-shard-00-02-pca9p.mongodb.net:27017/test?ssl=true&replicaSet=Ecommerce-shard-0&authSource=admin&retryWrites=true'
+}
+// const mongoose = require('mongoose');
+
+// const connect = (url) => mongoose.connect(url, {
+//     useNewUrlParser: true
+// })
+// module.exports = connect;
